1d1397df11b44ce5fdd32c0c450536d012f8c9eb
David Delgado <dadch13.14@gmail.com>
2020-01-18 22:55:41 -0400

    Métodos idempotentes del CRUD

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -19,6 +19,58 @@ function moviesApi(app) {
       next(error);
     }
   });
+
+  router.get('/:movieId', async function(req, res, next) {
+    try {
+      const movie = await Promise.resolve(moviesMock[0]);
+
+      res.status(200).json({
+        data: movie,
+        message: 'Movie retrieved'
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.post('/', async function(req, res, next) {
+    try {
+      const createdMovieId = await Promise.resolve(moviesMock[0].id);
+
+      res.status(201).json({
+        data: createdMovieId,
+        message: 'Movie created'
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.put('/:movieId', async function(req, res, next) {
+    try {
+      const updatedMovieId = await Promise.resolve(moviesMock[0].id);
+
+      res.status(200).json({
+        data: updatedMovieId,
+        message: 'Movie updated'
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.delete('/:movieId', async function(req, res, next) {
+    try {
+      const deletedMovieId = await Promise.resolve(moviesMock[0].id);
+
+      res.status(200).json({
+        data: deletedMovieId,
+        message: 'Movie deleted'
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
 }
 
 module.exports = moviesApi;
