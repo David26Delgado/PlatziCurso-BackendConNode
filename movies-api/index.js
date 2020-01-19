bb69f7e6a357739c9870a9538ed4bbed7d31c365
David Delgado <dadch13.14@gmail.com>
2020-01-19 15:19:04 -0400

    Capa de manejo de errores usando un middleware.

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
-
 const app = express();
 
 const { config } = require('./config/index');
-
 const moviesApi = require('./routes/movies');
+
+const { logErrors, errorHandler } = require('./utils/middlewares/errorHandler');
 
 // body parser - middleware
 app.use(express.json());
 
 moviesApi(app);
+
+// Middlewares de error siempre al final de las rutas
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
